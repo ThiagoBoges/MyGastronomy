7b1aca0752f60efa30c82508a82317fd2f1484cb
Thiago Borges <tbdsborges23@gmail.com>
2025-09-11 12:10:10 -0300

    feat: criação do mongo, importei no index.js e criei o .env para a conexão do banco de dados

---
+++ backend/src/database/mongo.js
@@ -0,0 +1,21 @@
+import { MongoClient } from 'mongodb';
+
+
+export const Mongo = {
+    async connect({ mongoConnectionString, mongoDBName }) {
+        try {
+              const client = new MongoClient(mongoConnectionString)
+
+              await client.connect()
+              const db = client.db(mongoDBName)
+
+              this.client = client 
+              this.db = db
+
+              return 'Connected to mongo database successfully!'
+
+        } catch (error) {
+            return { text: 'Error during mongo connection', error}
+        }
+    }
+}
